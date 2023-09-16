a7362cc1961c79a748a062cb4ddb596b87f3ad3b
Saheb Giri <iamsahebgiri@gmail.com>
2023-09-16 21:39:22 +0530

    chore: add basic layouts

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,19 +1,31 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import localFont from "next/font/local";
 import { type AppType } from "next/app";
 
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
 
+const inter = localFont({
+  src: "../fonts/inter-roman.var.woff2",
+});
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <style jsx global>{`
+        :root {
+          --inter-font: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 };
 
--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,9 +1,14 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
 import { type Config } from "tailwindcss";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--inter-font)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
