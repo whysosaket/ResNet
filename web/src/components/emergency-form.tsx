6fd71575f9ec167ce674febbb6bcaee227aa1268
Saheb Giri <iamsahebgiri@gmail.com>
2023-09-17 12:11:53 +0530

    feat:  now user can upload img from file + camera

--- web/src/components/emergency-form.tsx
+++ web/src/components/emergency-form.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-explicit-any */
@@ -5,28 +7,38 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import CameraCapture from "./camera-capture";
+import axios from "axios";
 
 import { RadioGroup, RadioGroupItem } from "@/components//ui/radio-group";
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from "@/components//ui/select";
 import { Button } from "@/components/ui/button";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Slider } from "@/components/ui/slider";
 import { Textarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
+import { v4 as uuidv4 } from "uuid";
+
+import { useRouter } from "next/router";
+import useGeolocation from "@/hooks/use-geolocation";
+import { useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 // import { toast } from "@/components/ui/use-toast"
+
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjUwNjRhZmZjNWU4MWMxNjQxZjk0NWM5In0sImlhdCI6MTY5NDkyNjg4Nn0._0Vx8Trpf5se14Mp07pa5PLFO1_fMEYNoq8CM1AqcDY";
 
 const FormSchema = z.object({
   severity: z.array(z.number().min(0).max(100)),
@@ -40,18 +52,72 @@ const FormSchema = z.object({
 });
 
 export default function EmergencyForm() {
+  const router = useRouter();
+  const geolocation = useGeolocation();
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
   });
 
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+
   function onSubmit(data: z.infer<typeof FormSchema>) {
-    console.log("Helllo");
-    console.log(data);
+    const requestData = {
+      category: [data.units],
+      location: [geolocation.latitude, geolocation.longitude],
+      victims: data.victims,
+      severity: data.severity.pop(),
+      details: data.details,
+      image: imageUrl,
+    };
+
+    axios
+      .post(
+        "http://localhost:9000/api/request",
+        {
+          ...requestData,
+        },
+        {
+          headers: {
+            "auth-token": token,
+          },
+        },
+      )
+      .then(() => {
+        void router.push("/success");
+        alert("We have informed the neared agency");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
+
+  const onUploadImage = async (url: string) => {
+    const blob = await (await fetch(url)).blob();
+    const formData = new FormData();
+    const file = new File([blob], `${uuidv4()}.jpg`, {
+      type: "image/jpeg",
+    });
+    formData.append("file", file);
+    console.log(formData.getAll("file"));
+
+    axios
+      .post(`http://localhost:9000/api/upload`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        const url = res.data.path as string;
+        setImageUrl(url);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <Form {...form}>
-      <CameraCapture />
+      <CameraCapture onUploadImage={onUploadImage} />
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
         <FormField
           control={form.control}
---
+++ web/src/pages/success.tsx
@@ -0,0 +1,37 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+
+export default function Success() {
+  return (
+    <div className="grid h-screen place-items-center">
+      <div className="p-6 md:mx-auto">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="mx-auto my-6 h-16 w-16 text-green-600"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2Zm0 1.5a8.5 8.5 0 1 0 0 17a8.5 8.5 0 0 0 0-17Zm-1.25 9.94l4.47-4.47a.75.75 0 0 1 1.133.976l-.073.084l-5 5a.75.75 0 0 1-.976.073l-.084-.073l-2.5-2.5a.75.75 0 0 1 .976-1.133l.084.073l1.97 1.97l4.47-4.47l-4.47 4.47Z"
+          />
+        </svg>
+        <div className="text-center">
+          <h3 className="text-center text-base font-bold md:text-2xl">
+            Informed successfully!
+          </h3>
+          <p className="text-muted-foreground my-2">
+            We have informed the required agency, they will be reaching anytime
+            soon.
+          </p>
+          <p className=" font-medium"> Have a great day! </p>
+          <div className="py-10 text-center">
+            <Link href="/" className={buttonVariants({ variant: "default" })}>
+              Go back
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
